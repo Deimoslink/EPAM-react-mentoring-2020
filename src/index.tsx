c78838ddce24c80a7e1832d300c0eceeb4bbeb90
Aleksandr_Linkov <aleksandr.linkov.consultant@consumer.org>
2020-05-28 00:04:27 +0300

    redux-persist added

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import * as ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
 import store from './core/store';
 import App from './containers/app/App';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistStore(store)}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('app')
 );
